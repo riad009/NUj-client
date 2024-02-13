d1382875950b7df3e1be293dd57179ac52890eb9
safwanbin2 <safwanridwan321@gmail.com>
2024-02-13 12:28:55 +0600

    hero - 1

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import court from "../../assets/home/court.mp4";
+import stone from "../../assets/home/stone.png";
+import { FcGoogle } from "react-icons/fc";
+import { BsApple } from "react-icons/bs";
+import { Button } from "antd";
 
 const Hero = () => {
-  return <div className="min-h-screen bg-primary">h</div>;
+  return (
+    <section className="bg-primary text-white h-auto md:h-screen flex justify-center items-center overflow-hidden">
+      <div className="w-11/12  mx-auto grid grid-cols-1 md:grid-cols-2 mt-20 ">
+        <div className="flex flex-col justify-center items-start gap-5">
+          <h1 className="text-6xl font-bold">Lorem, ipsum dolor.</h1>
+          <h1 className="text-6xl font-bold text-secondary">
+            Lorem, rem dolor.
+          </h1>
+          <p className="tracking-wide ">
+            Nuj is virtual restorative justice mobile eco-space monitoring
+            system offering cognitive monitoring & rehabilitative training
+            solution offering a fresh approach to monitored movement. It is a
+            fully-integrated mobile monitoring platform connecting courts,
+            electronic monitoring units with community-based organizational
+            support services in an all-in-one technology platform.
+          </p>
+          <div className="flex gap-2 ">
+            <Button
+              className="text-white"
+              size="large"
+              block
+              icon={<FcGoogle />}
+            >
+              Continue with Google
+            </Button>
+            <Button
+              className="text-white"
+              disabled
+              size="large"
+              block
+              icon={<BsApple />}
+            >
+              Continue with Apple
+            </Button>
+            {/* <button className="p-btn">Continue with </button>
+            <button className="p-btn">Try for free</button> */}
+          </div>
+          <p className="text-sm">VREMCAST is free to try</p>
+        </div>
+        <img className="w-[85%]" src={stone} alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
