bf3cd526f0d60bb089a71fa5b141ec8dbe33e7d1
mohammadJahid8 <mohammadjahid0007@gmail.com>
2024-03-03 19:29:08 +0600

    Upload

--- src/pages/toxicDetection/ToxicDetection.jsx
+++ src/pages/toxicDetection/ToxicDetection.jsx
@@ -1,6 +1,6 @@
 import { Drawer } from "antd";
 import { useState } from "react";
-import ToxicDetectionHistorySidebarItems from "./ToxicDetectionHistorySidebarItems";
+// import ToxicDetectionHistorySidebarItems from "./ToxicDetectionHistorySidebarItems";
 import ToxicDetectionResponseDisplay from "./ToxicDetectionResponseDisplay";
 import { MdHistory } from "react-icons/md";
 import axios from "axios";
@@ -8,7 +8,8 @@ import axios from "axios";
 const ToxicDetection = () => {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
-  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [responses, setResponses] = useState([]);
   const showDrawer = () => {
     setOpen(true);
   };
@@ -20,41 +21,37 @@ const ToxicDetection = () => {
     console.log(e.target.value);
   };
 
-  const handleGenerate = async () => {
-    try {
-      const payload = "test";
-
-      const res = await axios.post(
-        "http://localhost:5000/api/v1/eco-space-documents/toxicity-detection",
-        {
-          payload,
-        }
-      );
-      const response = res.data;
-
-      console.log({ response });
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
   const response = {
-    rating: 4,
-    plan: "Family Counseling",
-    psaTitle: "Effective Project Management",
-    psaIntroduction:
-      "This PSA highlights effective project management strategies, emphasizing the importance of addressing challenges collaboratively.",
-    suggestion:
-      "Your comprehensive success plan needs to include Family Counseling,Your comprehensive success plan needs to include Family CounselingYour comprehensive success plan needs to include Family CounselingYour comprehensive success plan needs to include Family CounselingYour comprehensive success plan needs to include Family Counseling",
+    rating: Number(responses[0]?.response.replace(/[^0-9]/g, "")),
+    plan: responses[1]?.response,
+    // psaTitle: "title",
+    psaIntroduction: responses[2]?.response,
+    suggestion: responses[3]?.response,
   };
 
-  const handleDetect = (e) => {
+  const handleDetect = async (e) => {
     e.preventDefault();
     if (text) {
-      setResult(response);
+      setLoading(true);
+      try {
+        const payload = "test";
+
+        const res = await axios.post(
+          "http://localhost:5000/api/v1/eco-space-documents/toxicity-detection",
+          {
+            payload,
+          }
+        );
+        const result = res.data.data;
+        setResponses(result);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.error("Error fetching data:", error);
+      }
     }
   };
-  console.log(result);
+
   return (
     <>
       <section className="min-h-screen flex justify-center items-center overflow-hidden">
@@ -74,30 +71,33 @@ const ToxicDetection = () => {
 
           <div className="flex flex-col gap-2">
             <form onSubmit={handleDetect} className="space-y-3 md:w-6/12">
-              <div className="flex flex-col gap-1 ">
+              {/* <div className="flex flex-col gap-1 ">
                 <label>Upload Pdf: </label>
                 <input
                   onChange={fileHandler}
                   type="file"
                   accept="application/pdf, application/vnd.ms-excel"
                 />
-              </div>
+              </div> */}
               <div className="flex flex-col gap-1 ">
                 <label>Text to process: </label>
                 <textarea
                   required
                   onChange={(e) => setText(e.target.value)}
                   className="h-28 bg-base-200 rounded-lg p-2 focus:outline-none"
-                  defaultValue={result?.text ? result?.text : ""}
                 ></textarea>
               </div>
-              <button className="p-btn" type="submit">
-                Detect
+              <button className="p-btn" type="submit" disabled={loading}>
+                {loading ? "Detecting.." : "Detect"}
               </button>
             </form>
           </div>
 
-          {result ? <ToxicDetectionResponseDisplay result={result} /> : ""}
+          {responses.length > 0 ? (
+            <ToxicDetectionResponseDisplay result={response} />
+          ) : (
+            ""
+          )}
         </div>
       </section>
       <Drawer
@@ -107,10 +107,10 @@ const ToxicDetection = () => {
         onClose={onClose}
         open={open}
       >
-        <ToxicDetectionHistorySidebarItems
+        {/* <ToxicDetectionHistorySidebarItems
           setResult={setResult}
           onClose={onClose}
-        />
+        /> */}
       </Drawer>
     </>
   );
