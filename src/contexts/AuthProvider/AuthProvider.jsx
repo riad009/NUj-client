d925d4a906b1423350b8c0687db3e53479968c83
safwanbin2 <safwanridwan321@gmail.com>
2024-03-03 19:34:43 +0600

    normal change

--- src/contexts/AuthProvider/AuthProvider.jsx
+++ src/contexts/AuthProvider/AuthProvider.jsx
@@ -57,6 +57,7 @@ const AuthProvider = ({ children }) => {
     return sendSignInLinkToEmail(auth, email, {
       // URL you want to redirect back to. The domain (www.example.com) for this
       // URL must be in the authorized domains list in the Firebase Console.
+      // url: "http://localhost:5173/",
       url: "http://localhost:5173/",
       // This must be true.
       handleCodeInApp: true,
