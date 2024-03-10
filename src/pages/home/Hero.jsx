be83609e105d9fab26d9a2190e9561fa277f4cbe
safwanbin2 <safwanridwan321@gmail.com>
2024-03-10 11:46:35 +0600

    hero-responsive

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -4,7 +4,7 @@ import banner from "../../assets/home/banner.png";
 const Hero = () => {
   return (
     <section className="bg-primary text-white h-auto md:h-screen flex justify-center items-center overflow-hidden">
-      <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-7 mt-8 gap-8">
+      <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-7 mb-8 md:mb-0 mt-20 md:mt-8 gap-8">
         <div className="flex flex-col justify-center items-start gap-5 md:col-span-2">
           <div className="">
             <h1 className="text-5xl font-bold !title-font">NUj</h1>
